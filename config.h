d277186973ce39634668b217e764604ce4d2c0e7
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-05-25 20:39:48 -0700

    Add center patch

--- config.h
+++ config.h
@@ -61,13 +61,13 @@ static const char *colors[][3] = {
 
 static const Rule rules[] = {
 
-	// { class              , instance              , title                 , tags mask, isfloating, monitor }
-	{ "st-256color"         , "fzfmenu"             , NULL                  , 0        , 1         , -1      },
-	{ "st-256color"         , "scratchpad"          , NULL                  , 0        , 1         , -1      },
-	{ "st-256color"         , "calculator"          , NULL                  , 0        , 1         , -1      },
-	{ "Pavucontrol"         , "pavucontrol"         , "Volume Control"      , 0        , 1         , -1      },
-	{ "SimpleScreenRecorder", "SimpleScreenRecorder", "simplescreenrecorder", 0        , 1         , -1      },
-	{ "Wicd-client.py"      , "wicd-client.py"      , "Wicd Network Manager", 0        , 1         , -1      },
+	// { class              , instance              , title                 , tags mask, iscentered, isfloating, monitor }
+	{ "st-256color"         , "fzfmenu"             , NULL                  , 0        , 1         , 1         , -1      },
+	{ "st-256color"         , "scratchpad"          , NULL                  , 0        , 1         , 1         , -1      },
+	{ "st-256color"         , "calculator"          , NULL                  , 0        , 1         , 1         , -1      },
+	{ "Pavucontrol"         , "pavucontrol"         , "Volume Control"      , 0        , 1         , 1         , -1      },
+	{ "SimpleScreenRecorder", "SimpleScreenRecorder", "simplescreenrecorder", 0        , 1         , 1         , -1      },
+	{ "Wicd-client.py"      , "wicd-client.py"      , "Wicd Network Manager", 0        , 1         , 1         , -1      },
 
 };
 
